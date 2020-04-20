8ab0db6b6f4c0af117deae972ddae0e24fcc9708
Tanner Williams <tannerwill756@gmail.com>
2020-04-20 19:15:00 -0400

    most tests done

---
+++ src/App.test.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { render, waitFor, queryAllByTestId } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import App from "./App";
+import { fetchShow as mockFetchShow } from "./api/fetchShow";
+
+jest.mock("./api/fetchShow");
+
+const episodesData = [
+  {
+    id: 553946,
+    url:
+      "http://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
+    name: "Chapter One: The Vanishing of Will Byers",
+    season: 1,
+    number: 1,
+    airdate: "2016-07-15",
+    airtime: "",
+    airstamp: "2016-07-15T12:00:00+00:00",
+    runtime: 60,
+    image: {
+      medium:
+        "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
+      original:
+        "http://static.tvmaze.com/uploads/images/original_untouched/67/168918.jpg",
+    },
+    summary:
+      "<p>A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.</p>",
+    _links: { self: { href: "http://api.tvmaze.com/episodes/553946" } },
+  },
+];
+
+test("App is rendering", () => {
+  mockFetchShow.mockResolvedValueOnce(episodesData);
+
+  const { getByText, queryAllByTestId, debug } = render(<App />);
+  debug();
+  // const select = getByText(/select a season/i);
+  // // console.log(select);
+  // userEvent.
+
+  // await waitFor(() => expect(queryAllByTestId(/episodes/i)).toHaveLength(1));
+});
