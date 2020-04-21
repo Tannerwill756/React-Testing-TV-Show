d3feeb7b324ce8df260fa6a1ffb3a93674d18bdc
Tanner Williams <tannerwill756@gmail.com>
2020-04-21 14:21:37 -0400

    mvp

--- src/App.test.js
+++ src/App.test.js
@@ -5,54 +5,24 @@ import userEvent from "@testing-library/user-event";
 import App from "./App";
 import { fetchShow as mockFetchShow } from "./api/fetchShow";
 
+import { dataSet } from "./dataSet";
+
 jest.mock("./api/fetchShow");
 
-const episodesData = [
-  {
-    id: 553946,
-    url:
-      "http://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
-    name: "Chapter One: The Vanishing of Will Byers",
-    season: 1,
-    number: 1,
-    airdate: "2016-07-15",
-    airtime: "",
-    airstamp: "2016-07-15T12:00:00+00:00",
-    runtime: 60,
-    image: {
-      medium:
-        "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
-      original:
-        "http://static.tvmaze.com/uploads/images/original_untouched/67/168918.jpg",
-    },
-    summary:
-      "<p>A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.</p>",
-    _links: { self: { href: "http://api.tvmaze.com/episodes/553946" } },
-  },
-];
-
-// test("App is rendering", () => {
-//   mockFetchShow.mockResolvedValueOnce(episodesData);
-
-//   const { getByText, queryAllByTestId, debug } = render(<App />);
-//   debug();
-//   const select = getByText(/select a season/i);
-//   // console.log(select);
-
-//   await waitFor(() => {expect(queryAllByTestId(/episodes/i)).toBeVisible();
-//   fireEvent.click(getByText(/select a season/i))
-//   });
-
-// });
-test("renders once loaded", async () => {
+test("renders my episodes once loaded and the season is selected", async () => {
   act(() => {
-    mockFetchShow.mockResolvedValueOnce(episodesData);
+    mockFetchShow.mockResolvedValueOnce(dataSet);
+  });
+  const { queryAllByTestId, getByText, queryByText, debug } = render(<App />);
 
-    const { getByPlaceholderText, queryByText } = render(<App />);
-    waitForElementToBeRemoved(queryByText(/Fetching data.../i)).then(() => {
-      const selection = getByPlaceholderText("Select an option");
-      selection.value = "Season 2";
-      expect(selection).toHaveValue("Season 2");
-    });
+  waitForElementToBeRemoved(queryByText(/Fetching data.../i)).then(() => {
+    const selection = getByText(/Select a season/i);
+    userEvent.click(selection, "Select a season");
+
+    const season = getByText(/season 1/i);
+
+    userEvent.click(season);
+
+    expect(queryAllByTestId(/episode/i)).toHaveLength(3);
   });
 });
